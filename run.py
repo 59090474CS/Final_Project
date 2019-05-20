4b2d4ac617d36f8173abb033df9ecb04b9865de1
cs59023458 <59023458@up.ac.th>
2019-05-21 03:45:25 +0700

    first commit

---
+++ run.py
@@ -0,0 +1,54 @@
+#import
+from flask import Flask,render_template,request,redirect,url_for
+import pymysql
+
+app = Flask(__name__)
+conn = pymysql.connect('localhost','root','','memberdb')
+
+@app.route("/")
+def showpage():
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM member")
+        rows = cur.fetchall()
+    return render_template('index.html',datas=rows)
+
+@app.route("/delete/<string:id_data>",methods=['GET'])
+def delete(id_data):
+    with conn:
+        cur = conn.cursor()
+        cur.execute("DELETE FROM member WHERE id=%s",(id_data))
+        conn.commit()
+    return redirect(url_for('showpage'))
+
+@app.route("/update",methods=['POST'])
+def update():
+    if request.method=="POST":
+        id_update = request.form['id']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        phone = request.form['phone']
+        with conn.cursor() as cursor:
+            sql = "UPDATE member SET fname=%s,lname=%s,phone=%s WHERE id=%s"
+            cursor.execute(sql,(fname,lname,phone,id_update))
+            conn.commit()
+        return redirect(url_for('showpage'))
+
+@app.route("/member")
+def addmember():
+    return render_template('add_member.html',)
+
+@app.route("/insert",methods=['POST'])
+def insert():
+    if request.method=="POST":
+        fname = request.form['fname']
+        lname = request.form['lname']
+        phone = request.form['phone']
+        with conn.cursor() as cursor:
+            sql = "INSERT INTO `member` (`fname`,`lname`,`phone`) values(%s,%s,%s)"
+            cursor.execute(sql,(fname,lname,phone))
+            conn.commit()
+        return redirect(url_for('showpage'))    
+
+if __name__ == "__main__":
+    app.run(debug=True)
